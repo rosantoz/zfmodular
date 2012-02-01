dbdb0599fc37e0c77b407d0df96b5b3ab67e280d
rosantoz <rodrigosamaster@gmail.com>
2012-02-01 21:37:45 -0200

    second commit

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Define path to application directory
+// Define path to the application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
